8d6d8e4918d96cfc156b803b2a151d47634c02e2
ataliarf <atalia.rf@gmail.com>
2022-01-01 15:51:27 +0200

    duplicate files

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Duplicate files
+
+This script scans a directory to find duplicate files based on their content.
+##### If the directory has files A, B, C, D, E, F and A+B+E have the same content and D+F have the same content, the expected output should be:
+##### * duplicate files: A, B, E
+##### * duplicate files: D, F
+# 
+#####  To perform the task, I decided to choose MD5. 
+##### MD5 is cryptographically hash function producing a 128-bit hash value.
+##### The script gets a path as a input. First, I checked if the path is valid.
+##### After it, I walk through all files within the folder and subfolders, mapping by size of each file to its path.
+##### The second stage was to walk through the keys of this map. For each key which is greather than 1, encode each of file from its files list, and put it in a map which is mapping by MD5 encode to its path.
+##### Now, I walked through the last map, printing duplicate files (i.e. each key that has more than 1 files in its values list).
+#
+The code assumes we have a read permittion to the files.
+
---
+++ dup.py.py
@@ -0,0 +1,71 @@
+
+import os, sys, hashlib
+
+#maps size of file to file path
+def mapBySizes(mainFolder):
+    mapOfSizes = {}
+    for root, dirs, files in os.walk(mainFolder):
+        for file in files:
+            path = os.path.join(root, file)
+            size = fileSize(path)
+            if size in mapOfSizes:
+                mapOfSizes[size].append(path)
+            else:
+                mapOfSizes[size] = [path]
+    return mapOfSizes
+
+def fileSize(path):
+    with open(path, 'rb') as f:
+        f.seek(0, os.SEEK_END)
+        size = f.tell()
+        f.close()
+    return size
+
+#maps hash of file to file path
+def mapByDupes(mapOfSizes):
+    dupesMap = {}
+    listsOfFiles = mapOfSizes.values()
+    for list in listsOfFiles:
+        if len(list) > 1:
+            for f in list:
+                file_hash = fileHash(f)
+                if file_hash in dupesMap:
+                    dupesMap[file_hash].append(f)
+                else:
+                    dupesMap[file_hash] = [f]
+    return dupesMap
+
+def fileHash(path):
+    with open(path, 'rb') as f:
+        bytes = f.read()
+        hash = hashlib.sha256(bytes).hexdigest()
+        f.close()
+    return hash 
+
+def printDuplicateFiles(dupesMap):
+    found = 0
+    values = dupesMap.values()
+    for listOfFiles in values:
+        if len(listOfFiles) > 1:
+            found = 1
+            names = map(lambda x: os.path.basename(x), listOfFiles)
+            print('* duplicate files:'),
+            print(', '.join(names))     
+    if found==0:               
+        print('no duplicate files.')
+ 
+if __name__ == '__main__':
+    argumentsNum = len(sys.argv)
+    if argumentsNum > 1:
+        sizes = {}
+        arg = sys.argv[1:]
+        path = arg[0]
+        if os.path.exists(path)== False : 
+            print('input is not a valid path')
+            sys.exit()
+        else:
+            sizes = mapBySizes(path)
+            duplicates = mapByDupes(sizes)
+            printDuplicateFiles(duplicates)
+    else:
+        print('please insert path')
